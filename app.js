5bf6271932161564085018e13ea93dec07e90145
Olga Sotula <olga.sotula@itctg.ru>
2022-08-25 15:59:24 +0300

    add models

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+});
+
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
