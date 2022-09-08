5bfee8e1b424a2d9c1de2cc521ccf923f7c4a836
Olga Sotula <olga.sotula@itctg.ru>
2022-09-08 21:34:53 +0300

    pw14 add createUser Joi validator

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,9 +4,9 @@ const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a
 
 const createUserValidator = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().regex(regex),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(regex),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
