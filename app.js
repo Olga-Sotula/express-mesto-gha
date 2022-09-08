eed984241192867cab8efb4eb3d5cf2c331c59a6
Olga Sotula <olga.sotula@itctg.ru>
2022-09-08 15:47:38 +0300

    pw14.8 remove hardcode userId

--- app.js
+++ app.js
@@ -20,14 +20,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '63077aa6994df69055a6ffb9',
-  };
-
-  next();
-});
-
 app.use(express.json());
 app.use(cookieParser());
 
