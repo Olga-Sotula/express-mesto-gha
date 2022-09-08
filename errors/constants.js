932da68da45d853af4d54a711508a122d2a306a5
Olga Sotula <olga.sotula@itctg.ru>
2022-09-08 15:55:49 +0300

    pw14.11.1 update err code

--- errors/constants.js
+++ errors/constants.js
@@ -1,11 +1,17 @@
-const StatusOk = 200;
-const StatusBadRequest = 400;
-const StatusNotFound = 404;
-const StatusServerError = 500;
+const STATUS_OK = 200;
+const STATUS_BAD_REQUEST = 400;
+const STATUS_BAD_AUTH = 401;
+const STATUS_FORBIDDEN = 403;
+const STATUS_NOT_FOUND = 404;
+const STATUS_EMAIL_DUBLICATE = 409;
+const STATUS_SERVER_ERR = 500;
 
 module.exports = {
-  StatusOk,
-  StatusBadRequest,
-  StatusNotFound,
-  StatusServerError,
+  STATUS_OK,
+  STATUS_BAD_REQUEST,
+  STATUS_BAD_AUTH,
+  STATUS_FORBIDDEN,
+  STATUS_NOT_FOUND,
+  STATUS_EMAIL_DUBLICATE,
+  STATUS_SERVER_ERR,
 };
