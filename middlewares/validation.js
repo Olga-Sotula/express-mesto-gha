69da6d28334604260bdce6036090f7869610867f
Olga Sotula <olga.sotula@itctg.ru>
2022-09-08 21:07:45 +0300

    pw14 add createUser Joi validator

---
+++ middlewares/validation.js
@@ -0,0 +1,12 @@
+const { celebrate, Joi } = require('celebrate');
+
+const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+
+const createUserValidator = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(regex).message('Невалидный аватар'),
+  }),
+});
+
+module.exports = { createUserValidator };
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
+const { createUserValidator } = require('../middlewares/validation');
+
 const authRouter = express.Router();
 const {
   createUser,
   login,
 } = require('../controllers/users');
 
-authRouter.post('/signup', createUser);
+authRouter.post('/signup', createUserValidator, createUser);
 authRouter.post('/signin', login);
 
 module.exports = {
